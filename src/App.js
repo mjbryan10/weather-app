bb9186ea522fca50e07e9ee91b030e109f11ec71
mjbryan <mjbyran10@gmail.com>
2020-02-10 14:25:47 +0100

    conditional render for alerts

--- src/App.js
+++ src/App.js
@@ -1,29 +1,29 @@
 import React, { Component } from "react";
 import "./App.scss";
 
-import Data from './resources/data/data'
+import Data from "./resources/data/data";
 
 import Header from "./components/layout/Header";
-import Alert from './components/Alert'
+import Alert from "./components/Alert";
 import Question from "./components/Question";
 
 export default class App extends Component {
 	state = {
-    data: Data,
-    datastate: "loading",
-    result: "" //returned result from Question: can be rain, chance or sunny
+		data: Data,
+		// data: {},
+		datastate: "loading", //"ready", "loading", "none"
+		result: "", //returned result from Question: can be rain, chance or sunny
 	};
 	render() {
 		return (
 			<div className="app-container">
-        {console.log(this.state.data)}
-        <Alert alerts={this.state.data.alerts}></Alert>
+				{this.state.data.alerts ? <Alert alerts={this.state.data.alerts}></Alert> : ""}
 				<Header></Header>
 				<div className="content-container">
 					<Question></Question>
 					<p>{this.state.datastate === "ready" ? <span>"Hello World"</span> : ""}</p>
 				</div>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.47/js/bootstrap-datetimepicker.min.js"></script>
+				<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.47/js/bootstrap-datetimepicker.min.js"></script>
 			</div>
 		);
 	}
